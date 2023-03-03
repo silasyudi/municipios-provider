b2503983e7113e8be5ba3a66b64f222ae539d48f
Silas Yudi <syudik12@gmail.com>
2023-03-03 11:30:21 -0300

    Mock third-party api

--- tests/Feature/IntegrationTest.php
+++ tests/Feature/IntegrationTest.php
@@ -2,46 +2,32 @@
 
 namespace Tests\Feature;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Env;
 use Tests\TestCase;
 
 class IntegrationTest extends TestCase
 {
-    private const RR_IBGE = [
-        ['ibge_code' => '1400027', 'name' => 'Amajari',],
-        ['ibge_code' => '1400050', 'name' => 'Alto Alegre',],
-        ['ibge_code' => '1400100', 'name' => 'Boa Vista',],
-        ['ibge_code' => '1400159', 'name' => 'Bonfim',],
-        ['ibge_code' => '1400175', 'name' => 'Cantá',],
-        ['ibge_code' => '1400209', 'name' => 'Caracaraí',],
-        ['ibge_code' => '1400233', 'name' => 'Caroebe',],
-        ['ibge_code' => '1400282', 'name' => 'Iracema',],
-        ['ibge_code' => '1400308', 'name' => 'Mucajaí',],
-        ['ibge_code' => '1400407', 'name' => 'Normandia',],
-        ['ibge_code' => '1400456', 'name' => 'Pacaraima',],
-        ['ibge_code' => '1400472', 'name' => 'Rorainópolis',],
-        ['ibge_code' => '1400506', 'name' => 'São João da Baliza',],
-        ['ibge_code' => '1400605', 'name' => 'São Luiz',],
-        ['ibge_code' => '1400704', 'name' => 'Uiramutã',],
+    private const RESPONSE_IBGE
+        = '[{"id":"1","nome":"João Pessoa"},{"id":"2","nome":"Campina Grande"}]';
+    private const RESPONSE_BRASIL_API
+        = '[{"codigo_ibge":"1","nome":"JOAO PESSOA"},{"codigo_ibge":"2","nome":"CAMPINA GRANDE"}]';
+
+    private const EXPECTED_IBGE = [
+        ['ibge_code' => '1', 'name' => 'João Pessoa',],
+        ['ibge_code' => '2', 'name' => 'Campina Grande',],
     ];
 
-    private const RR_BRASIL_API = [
-        ['ibge_code' => '1400027', 'name' => 'AMAJARI',],
-        ['ibge_code' => '1400050', 'name' => 'ALTO ALEGRE',],
-        ['ibge_code' => '1400100', 'name' => 'BOA VISTA',],
-        ['ibge_code' => '1400159', 'name' => 'BONFIM',],
-        ['ibge_code' => '1400175', 'name' => 'CANTA',],
-        ['ibge_code' => '1400209', 'name' => 'CARACARAI',],
-        ['ibge_code' => '1400233', 'name' => 'CAROEBE',],
-        ['ibge_code' => '1400282', 'name' => 'IRACEMA',],
-        ['ibge_code' => '1400308', 'name' => 'MUCAJAI',],
-        ['ibge_code' => '1400407', 'name' => 'NORMANDIA',],
-        ['ibge_code' => '1400456', 'name' => 'PACARAIMA',],
-        ['ibge_code' => '1400472', 'name' => 'RORAINOPOLIS',],
-        ['ibge_code' => '1400506', 'name' => 'SAO JOAO DA BALIZA',],
-        ['ibge_code' => '1400605', 'name' => 'SAO LUIZ',],
-        ['ibge_code' => '1400704', 'name' => 'UIRAMUTA',],
+    private const EXPECTED_BRASIL_API = [
+        ['ibge_code' => '1', 'name' => 'JOAO PESSOA',],
+        ['ibge_code' => '2', 'name' => 'CAMPINA GRANDE',]
     ];
+
+    private array $history = [];
 
     public function test_invalid_uf_returns_404(): void
     {
@@ -52,26 +38,53 @@ class IntegrationTest extends TestCase
     public function test_valid_uf_with_ibge_provider_returns_200(): void
     {
         Env::getRepository()->set('PROVIDER', 'IBGE');
-        $response = $this->get('/api/municipio/rr');
-        $response->assertJsonCount(15);
-        $response->assertExactJson(self::RR_IBGE);
+        $this->mockThirdPartyApi(self::RESPONSE_IBGE);
+
+        $response = $this->get('/api/municipio/pb');
+        $response->assertJsonCount(2);
+        $response->assertExactJson(self::EXPECTED_IBGE);
         $response->assertOk();
+
+        $this->assertCount(1, $this->history);
+        $this->assertEquals('servicodados.ibge.gov.br', $this->history[0]['request']->getUri()->getHost());
     }
 
     public function test_valid_uf_with_brasil_api_provider_returns_200(): void
     {
         Env::getRepository()->set('PROVIDER', 'BRASIL_API');
-        $response = $this->get('/api/municipio/rr');
-        $response->assertJsonCount(15);
-        $response->assertSimilarJson(self::RR_BRASIL_API);
+        $this->mockThirdPartyApi(self::RESPONSE_BRASIL_API);
+
+        $response = $this->get('/api/municipio/pb');
+        $response->assertJsonCount(2);
+        $response->assertExactJson(self::EXPECTED_BRASIL_API);
         $response->assertOk();
+
+        $this->assertCount(1, $this->history);
+        $this->assertEquals('brasilapi.com.br', $this->history[0]['request']->getUri()->getHost());
     }
 
     public function test_valid_uf_with_invalid_provider_returns_400(): void
     {
         Env::getRepository()->set('PROVIDER', 'invalid');
-        $response = $this->get('/api/municipio/rr');
+        $response = $this->get('/api/municipio/pb');
         $response->assertServerError();
+    }
+
+    private function mockThirdPartyApi(string $responseBody): void
+    {
+        $mock = new MockHandler([
+            new Response(200, [], $responseBody)
+        ]);
+
+        $this->history = [];
+        $history = Middleware::history($this->history);
+
+        $handler = HandlerStack::create($mock);
+        $handler->push($history);
+
+        $guzzle = new Client(['handler' => $handler]);
+
+        $this->app->instance(Client::class, $guzzle);
     }
 }
 
